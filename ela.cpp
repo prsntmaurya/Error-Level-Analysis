164015e3accec712cca07edee8018a9a02edea45
prsntmaurya <prsntmaurya@gmail.com>
2017-11-26 23:01:20 +0530

    added image compression

--- ela.cpp
+++ ela.cpp
@@ -1,13 +1,22 @@
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc.hpp"
 #include<iostream>
 using namespace cv;
 using namespace std;
 
 int main(){
-	Mat img,resaved_img;
+	Mat img,compressed_img,compressed_img_resized;
 	img=imread("original.jpg");
-	imwrite("resaved.jpg",img);
-	resaved_img=imread("resaved.jpg");
-	imshow("resaved image",resaved_img);
+
+	//compresing image by certain factor ie: lossy compression
+	vector<int> compressing_factor;
+	compressing_factor.push_back(CV_IMWRITE_JPEG_QUALITY);
+	compressing_factor.push_back(90);
+
+	imwrite("compressed.jpg",img);
+	compressed_img=imread("compressed.jpg");
+	//resize image since the image was of higher resolution
+	resize(compressed_img, compressed_img_resized, Size(),0.30,0.30);
+	imshow("compressed image",compressed_img_resized);
 	waitKey(0);
 }
